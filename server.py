9aa17af26bd30633bab3160cc9c93d4af786fabc
sskruger23 <sskruger.23@gmail.com>
2025-10-09 16:38:28 -0600

    vercel2

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import requests
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/remixback": {"origins": "verdant-pie-836149.netlify.app"}})
+CORS(app, resources={r"/remixback": {"origins": "remixback.vercel.app"}})
 
 API_KEY = os.getenv('GENERATIVE_API_KEY')
 if not API_KEY:
